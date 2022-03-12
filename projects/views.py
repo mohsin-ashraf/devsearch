dde9ec72ad38b3cd6c7297288b699df61da3f4af
mohsin <mohsin.ashraf12@gmail.com>
2022-03-12 15:59:16 +0500

    adding crud functionality

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from operator import methodcaller
+from django.shortcuts import render, redirect
 from .models import Project
-
+from .forms import ProjectForm
 
 def projects(request):
     projects = Project.objects.all()
@@ -11,3 +12,37 @@ def project(request, pk):
     single_project = Project.objects.get(id=pk)
     tags = single_project.tags.all()
     return render(request, "projects/single-project.html", {"project": single_project, "tags": tags})
+
+def create_project(request):
+    form = ProjectForm()
+    
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+     
+    context = {"form":form}
+    return render(request, "projects/project_form.html", context)
+
+def update_project(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+
+    context = {"form":form}
+    return render(request, "projects/project_form.html", context)
+
+def delete_project(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == "POST":
+        project.delete()
+        return redirect("projects")
+
+    context = {"object":project}
+    return render(request, "projects/delete_template.html", context)
